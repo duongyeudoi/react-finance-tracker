3cca1fdbd086e03e94f00b91f5e5526ba9460e47
duongyeudoi <19020265@vnu.edu.vn>
2022-05-05 15:31:48 +0700

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
@@ -6,12 +7,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <nav>
-          <h1>Finance Tracker</h1>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </nav>
+        <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
