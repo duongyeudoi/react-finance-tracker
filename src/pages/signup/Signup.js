197fb8d4db2b0f5fac8e58fb3e3b24f9b4f156f0
duongyeudoi <19020265@vnu.edu.vn>
2022-05-05 23:23:58 +0700

    add signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,45 @@
 import styles from './Signup.module.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, name);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Signup</h2>
+      <label>
+        <span>Display name:</span>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Submit</button>
+    </form>
+  );
 }
