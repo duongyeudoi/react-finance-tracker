89b459cf7f15580c97822359ea42a7fc964f1d79
duongyeudoi <19020265@vnu.edu.vn>
2022-05-05 15:25:44 +0700

    setup pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css'
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <nav>
+          <h1>Finance Tracker</h1>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
