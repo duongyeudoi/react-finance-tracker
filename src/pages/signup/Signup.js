67b30f86bb4f15778b6dd3cd1a45b8ee9d69649c
duongyeudoi <19020265@vnu.edu.vn>
2022-05-08 23:39:17 +0700

    use signup hook

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,15 +1,16 @@
 import styles from './Signup.module.css';
-
+import { useSignup } from '../../hooks/useSignup';
 import React, { useState } from 'react';
 
 export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
+  const { error, isPending, signup } = useSignup();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, name);
+    signup(email, password, name);
   };
 
   return (
@@ -39,7 +40,14 @@ export default function Signup() {
           value={password}
         />
       </label>
-      <button className="btn">Submit</button>
+      {!isPending && <button className="btn">Submit</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+
+      {error && <p>{error}</p>}
     </form>
   );
 }
